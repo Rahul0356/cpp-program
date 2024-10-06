16df2ca0af3333c39d844b2f78827c5cdb963bc1
rahul <rahulkumar9199270356@gmail.com>
2024-10-06 12:58:08 +0530

    init

---
+++ marks.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main( ){
+int marks;
+cout<<"enter marks:";
+cin>>marks;
+if(marks>=98){
+    cout <<"Rahul\n";
+} else if(marks<=99 && marks>=99){
+    cout<<"b\n";
+}else{
+    cout <<"c\n";
+}
+return 0;
+}
